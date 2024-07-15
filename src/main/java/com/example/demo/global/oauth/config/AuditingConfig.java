26718e36d605e776b0b4f7bb74b71db72a3fe391
min2h <mhlee1304@naver.com>
2024-07-15 21:40:41 +0900

    Fix: AuditingConfig 관련 설명 주석

--- src/main/java/com/example/demo/global/oauth/config/AuditingConfig.java
+++ src/main/java/com/example/demo/global/oauth/config/AuditingConfig.java
@@ -3,6 +3,11 @@ package com.example.demo.global.oauth.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+/***
+ * 테스트코드 실행 중 발생하는 오류로 인해
+ * JPA Auditing Config 생성
+ */
 @Configuration
 @EnableJpaAuditing // JPA Auditing 활성화
 public class AuditingConfig {
